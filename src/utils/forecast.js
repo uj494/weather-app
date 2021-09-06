5a6b1544852af848c78eac6afba667147c0ecda0
Ujjwal Soni <ujjwal.soni494@gmail.com>
2021-09-06 21:18:08 +0530

    Add forecast text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, long, callback) => {
         } else if(body.error) {
             callback('Unable to find location!', undefined)
         }  else {
-            callback(undefined, body.current.weather_descriptions[0] + ' in ' + body.location.name + '. It is currently ' + body.current.temperature + ' degrees' + ' out. It feels like  ' + body.current.feelslike + ' degrees' + ' out.')
+            callback(undefined, body.current.weather_descriptions[0] + ' in ' + body.location.name + '. It is currently ' + body.current.temperature + ' degrees' + ' out. It feels like  ' + body.current.feelslike + ' degrees' + ' out. Humidity is '+body.current.humidity+'%.')
         }
     })
 }
